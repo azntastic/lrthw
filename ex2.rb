659823a2d4a4acf976c3fe1f0e3bdcfd67cbb295
Ryan <Ryanchu.ws@gmail.com>
2017-01-26 19:43:49 +0000

    First Commit

---
+++ ex2.rb
@@ -0,0 +1,7 @@
+# This is a comment
+
+puts "I could have code like this." # Bleep Bloop
+
+# puts "Holy Smokes."
+
+puts "This will run."
---
+++ ex3.rb
@@ -0,0 +1,24 @@
+puts "I will now count my dinosaurs:"
+#prints the string to console and adds a new line
+puts "Raptors #{25 + 30 / 6}"
+#prints string, but runs operation between {} + newline. / is divided
+puts "Brontosaurus #{100 - 25 * 3 % 4}"
+#% is remainder(?)
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+#if one uses quotes the entire input following will be treated as a string
+puts 3 + 2 < 5 - 7
+#< comparison operator, ruby compares both sides of equation.
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= 2}"
+puts "Is it less or equal? #{5 <= -2}"
