52f00784ff2d06379771ae4b002d9b78eece3482
Ryan <Ryanchu.ws@gmail.com>
2017-01-27 23:03:36 +0000

    Second Commit

---
+++ ex5.rb
@@ -0,0 +1,16 @@
+my_name = 'Ryan C.'
+my_age = 22
+my_height = 185 #cm
+my_weight = 83 #kg
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} cm tall."
+puts "He's #{my_weight} kg heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}." 
